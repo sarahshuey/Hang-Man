1073083bf676ca9ec3eab834542ab4410eda7017
Sarah Shuey <sarah.shuey@me.com>
2017-08-18 14:27:17 -0500

    added session

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
-const mustacheExpress = require('mustache-express');
+const mustacheexpress = require('mustache-express');
+const bodyparser = require('body-parser');
+const session = require('express-session');
 const app = express();
 
 
-app.engine('mustache', mustacheExpress());
+// this is looking for mustache code in my views folder
+app.engine('mustache', mustacheexpress());
 app.set('views','./views');
 app.set('view engine','mustache');
 
-
+// must do for session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+// this is going to the index.mustache page
 app.get('/',function(req,res){
   res.render('index')
 })
